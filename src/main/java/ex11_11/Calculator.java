bd1c23c6b9dc9b97efc573e3ca91e551133f031c
slawo <slawekpettke@gmail.com>
2022-08-07 19:21:34 +0200

    ---

--- src/main/java/ex11_11/Calculator.java
+++ src/main/java/ex11_11/Calculator.java
@@ -1,4 +1,22 @@
 package ex11_11;
 
 public class Calculator {
+
+    public int factorial(int num) {
+
+        int answer = 1;
+        for (int i = 1; i <= num; i++) {
+            answer *= i;
+        }
+
+        return answer;
+    }
+
+    public int binomialCoefficent(int setSize, int subsetSize) {
+
+        int numerator = factorial(setSize);
+        int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
+
+        return numerator / denominator;
+    }
 }
