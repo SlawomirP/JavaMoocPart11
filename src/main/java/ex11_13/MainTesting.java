3c35c6a55c6dc41abf2028fbd3ccd69a0f79a8d1
slawo <slawekpettke@gmail.com>
2022-08-08 20:08:32 +0200

    ---

---
+++ src/main/java/ex11_13/MainTesting.java
@@ -0,0 +1,15 @@
+package ex11_13;
+
+public class MainTesting {
+    public static void main(String[] args) {
+        SaveableDictionary dictionary = new SaveableDictionary();
+        dictionary.add("apina", "monkey");
+        dictionary.add("banaani", "banana");
+        dictionary.add("apina", "apfe");
+
+        System.out.println(dictionary.translate("apina"));
+        System.out.println(dictionary.translate("monkey"));
+        System.out.println(dictionary.translate("ohjelmointi"));
+        System.out.println(dictionary.translate("banana"));
+    }
+}
--- src/main/java/ex11_13/SaveableDictionary.java
+++ src/main/java/ex11_13/SaveableDictionary.java
@@ -1,4 +1,41 @@
 package ex11_13;
 
+import java.util.HashMap;
+import java.util.Map;
+
+//STWORZENIE SLOWNICZKA MAJACEGO MOZLIWOSC ZAPISYWANIA I ODCZYTU PLIKU
+//SLOWNICZEK MUSI TLUMACZYC Z FINSKIEGO NA INNY JEZYI NA ODWROT
 public class SaveableDictionary {
+
+    //TWORZE HASHMAPE DO PRZECHOWYWANIA ALOW I ICH TLUMACZEN
+    private HashMap<String, String> dictionary;
+
+    public SaveableDictionary () {
+        this.dictionary = new HashMap<>();
+    }
+
+    //METODA DODAJACA DO SLOWNIKA SLOWO I JEGO TLUMACZENIE
+    //NIE MOZE BYC MOZLIWOSCI DODANIA DRUGI RAZ TEGO SAMEGO SLOWA
+    public void add (String word, String translation){
+        if (!this.dictionary.containsKey(word)){
+            this.dictionary.put(word,translation);
+        }
+    }
+    //METODA ZWROCI TLUMACZENIE WPISANEGO SLOWA, JEZELI SLOWA
+    //NIE MA W SLOWNIKU TO ZWROCI NULL
+    public String translate (String word){
+        if (!this.dictionary.containsKey(word) || !this.dictionary.containsValue(word)){
+            return null;
+        }
+        if(this.dictionary.containsValue(word)){
+            //METODA NA WYCIAGNIĘCIE KLUCZA MAJAC WARTOSC Z POD TEGO KLUCZA
+            for(Map.Entry<String, String> entry: dictionary.entrySet()){
+                if(entry.getValue().equals(word)){
+                    return entry.getKey();
+                }
+            }
+        }
+        return this.dictionary.get(word);
+    }
+
 }
