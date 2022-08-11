df50aefba83061857c60b3c694eceaee9f79a9b2
slawo <slawekpettke@gmail.com>
2022-08-11 22:25:18 +0200

    ---

--- src/main/java/ex12_03/Program.java
+++ src/main/java/ex12_03/Program.java
@@ -6,13 +6,7 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int[] numbers = {3, -1, 8, 4};
-
-        System.out.println(sum(numbers, 0, 0, 0, 0));
-        System.out.println(sum(numbers, 0, 0, 0, 10));
-        System.out.println(sum(numbers, 0, 1, 0, 10));
-        System.out.println(sum(numbers, 0, 1, -10, 10));
-        System.out.println(sum(numbers, -1, 999, -10, 10));
+        int[] numbers = {8, -2, 3, 1, 1}; System.out.println(sum(numbers, 0, numbers.length, 0, 999));
 
 
     }
