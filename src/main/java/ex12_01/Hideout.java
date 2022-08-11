38adf4b83ca208ca8f6f9aa1dcc9efae0578786d
slawo <slawekpettke@gmail.com>
2022-08-11 17:13:11 +0200

    ---

--- src/main/java/ex12_01/Hideout.java
+++ src/main/java/ex12_01/Hideout.java
@@ -12,9 +12,38 @@ public class Hideout<T> {
     }
 
     //metoda ktora dodaje obiekt do listy, w liscie moze być max 1 obiekt
-    //jezeli dodajemy obiekt ktory jest juz w liscie
-    //ten obiekt znika
+    //jezeli dodajemy obiekt a jest juz cos w liscie
+    //ten obiekt znika - zostaje nadpisany
     public void putIntoHideout(T toHide){
-
+        if(!this.hide.isEmpty()){
+            this.hide.clear();
+            this.hide.add(toHide);
+        } else {
+            this.hide.add(toHide);
+        }
     }
+    // ZWRACA OBIEKT Z LISTY, JEZELI LISTA JEST PUSTA
+    //TO ZWROC NULLA
+    public T takeFromHideout(){
+        if (this.hide.isEmpty()){
+            return null;
+        }
+        //JEZELI COS JEST W LISCIE TO PRZYPISUJE TO DO
+        //POMOCNICZEGO OBIEKTU, USUWAM Z LISTY I ZWRACAM
+        //POMOCNICZY OBIEKT
+        T helper =  this.hide.get(0);
+        this.hide.remove(0);
+        return helper;
+    }
+//SPRAWDZA CZY COS JEST W LICIE
+    public boolean isInHideout(){
+        if(!this.hide.isEmpty()){
+            return true;
+        } else{
+         return false;
+        }
+    }
+
+
+
 }
---
+++ src/main/java/ex12_01/MainForTests.java
@@ -0,0 +1,17 @@
+package ex12_01;
+
+public class MainForTests {
+    public static void main(String[] args) {
+        Hideout<String> den = new Hideout<>();
+        System.out.println(den.isInHideout());
+        den.putIntoHideout("peekaboo");
+        System.out.println(den.isInHideout());
+        System.out.println(den.takeFromHideout());
+        System.out.println(den.isInHideout());
+        den.putIntoHideout("toodaloo");
+        den.putIntoHideout("heelloo");
+        System.out.println(den.isInHideout());
+        System.out.println(den.takeFromHideout());
+        System.out.println(den.isInHideout());
+    }
+}
