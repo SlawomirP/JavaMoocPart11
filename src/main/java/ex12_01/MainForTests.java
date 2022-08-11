588e331d507582c9ebce1586bc708dc9f1ce00bb
slawo <slawekpettke@gmail.com>
2022-08-11 17:30:24 +0200

    ---

--- src/main/java/ex12_01/MainForTests.java
+++ src/main/java/ex12_01/MainForTests.java
@@ -2,14 +2,10 @@ package ex12_01;
 
 public class MainForTests {
     public static void main(String[] args) {
-        Hideout<String> den = new Hideout<>();
+        Hideout<Integer> den = new Hideout<>();
         System.out.println(den.isInHideout());
-        den.putIntoHideout("peekaboo");
+        den.putIntoHideout(1);
         System.out.println(den.isInHideout());
-        System.out.println(den.takeFromHideout());
-        System.out.println(den.isInHideout());
-        den.putIntoHideout("toodaloo");
-        den.putIntoHideout("heelloo");
         System.out.println(den.isInHideout());
         System.out.println(den.takeFromHideout());
         System.out.println(den.isInHideout());
