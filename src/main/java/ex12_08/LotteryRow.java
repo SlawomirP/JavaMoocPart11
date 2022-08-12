bdc7701f4c7a4f80b03e1b5e378f51f4a1328e0c
slawo <slawekpettke@gmail.com>
2022-08-12 18:34:04 +0200

    ---

--- src/main/java/ex12_08/LotteryRow.java
+++ src/main/java/ex12_08/LotteryRow.java
@@ -4,9 +4,11 @@ package ex12_08;
 import java.util.ArrayList;
 import java.util.Random;
 
+//LOSOWANIE 7 RÓŻNYCH LICZB Z ZAKRESU 1-40
 public class LotteryRow {
 
     private ArrayList<Integer> numbers;
+    private Random r = new Random();
 
     public LotteryRow() {
         // Draw the numbers when the LotteryRow is created
@@ -20,12 +22,23 @@ public class LotteryRow {
     public void randomizeNumbers() {
         // Initialize the list for numbers
         this.numbers = new ArrayList<>();
-        // Implement the random number generation here
-        // the method containsNumber is probably useful
+        while (this.numbers.size() == 7) {
+            int drawNumber = r.nextInt(1, 41);
+            if (!this.numbers.contains(drawNumber)) {
+                this.numbers.add(drawNumber);
+            }
+        }
     }
+    // Implement the random number generation here
+    // the method containsNumber is probably useful
+
 
     public boolean containsNumber(int number) {
-        // Check here whether the number is among the drawn numbers
+        for (Integer num : this.numbers) {
+            if (num == number) {
+                return true;
+            }
+        }
         return false;
     }
 }
