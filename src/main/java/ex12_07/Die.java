8b82703d2a0b171c9c8fb11dc9e25bbdc8241418
slawo <slawekpettke@gmail.com>
2022-08-12 18:16:37 +0200

    ---

---
+++ src/main/java/ex12_07/Die.java
@@ -0,0 +1,17 @@
+package ex12_07;
+import java.util.Random;
+
+public class Die {
+
+    private Random random;
+    private int numberOfFaces;
+
+    public Die(int numberOfFaces) {
+        this.random = new Random();
+        this.numberOfFaces = numberOfFaces;
+    }
+
+    public int throwDie() {
+        return random.nextInt(1, this.numberOfFaces + 1);
+    }
+}
---
+++ src/main/java/ex12_07/Program.java
@@ -0,0 +1,11 @@
+package ex12_07;
+
+public class Program {
+    public static void main(String[] args) {
+        Die die = new Die(6);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(die.throwDie());
+        }
+    }
+}
